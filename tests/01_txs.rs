59c3bd0d479867d5bad79f7d3a296c8246409117
Thorsten Lorenz <thlorenz@gmx.de>
2025-06-20 12:06:16 +0800

    chore: more success tx tests

--- tests/01_txs.rs
+++ tests/01_txs.rs
@@ -1,5 +1,6 @@
 use log::*;
-use solana_sdk::{hash::Hash, message::VersionedMessage};
+use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{hash::Hash, message::VersionedMessage, transaction::TransactionError};
 use solana_transaction_status::{TransactionConfirmationStatus, TransactionStatus};
 use std::sync::Arc;
 
@@ -24,8 +25,40 @@ fn successful_transaction_status() -> Option<TransactionStatus> {
     })
 }
 
+fn create_account_tx() -> VersionedTransaction {
+    let auth = Keypair::new();
+    let new_account = Keypair::new();
+    let ix = system_instruction::create_account(
+        &auth.pubkey(),
+        &new_account.pubkey(),
+        1_000_000_000,
+        0,
+        &system_program::ID,
+    );
+
+    let versioned_msg = Message::try_compile(&auth.pubkey(), &[ix], &[], Hash::default()).unwrap();
+
+    VersionedTransaction::try_new(VersionedMessage::V0(versioned_msg), &[&auth, &new_account])
+        .unwrap()
+}
+
+async fn sig_status(rpc_client: &RpcClient, sig: Signature) -> Result<(), TransactionError> {
+    rpc_client
+        .get_signature_status(&sig)
+        .await
+        .unwrap()
+        .unwrap()
+}
+
+async fn sig_statuses(
+    rpc_client: &RpcClient,
+    sigs: &[Signature],
+) -> Vec<Option<TransactionStatus>> {
+    rpc_client.get_signature_statuses(sigs).await.unwrap().value
+}
+
 #[tokio::test]
-async fn test_all_txs_success() {
+async fn test_one_tx_success() {
     struct AllTxsSuccessMocker;
     impl SoxMocker for AllTxsSuccessMocker {
         fn handle_transaction(&self, tx: VersionedTransaction) -> Option<TransactionResult> {
@@ -40,39 +73,51 @@ async fn test_all_txs_success() {
     let (url, handle) = utils::start(mocker).await.unwrap();
     let rpc_client = utils::create_rpc_client(&url);
 
-    let auth = Keypair::new();
-    let new_account = Keypair::new();
-    let ix = system_instruction::create_account(
-        &auth.pubkey(),
-        &new_account.pubkey(),
-        1_000_000_000,
-        0,
-        &system_program::ID,
+    let tx = create_account_tx();
+    let sig = rpc_client.send_and_confirm_transaction(&tx).await.unwrap();
+    let (status, statuses) = (
+        sig_status(&rpc_client, sig).await,
+        sig_statuses(&rpc_client, &[sig]).await,
     );
 
-    let versioned_msg = Message::try_compile(&auth.pubkey(), &[ix], &[], Hash::default()).unwrap();
-    let versioned_tx =
-        VersionedTransaction::try_new(VersionedMessage::V0(versioned_msg), &[&auth, &new_account])
-            .unwrap();
-    let sig = rpc_client
-        .send_and_confirm_transaction(&versioned_tx)
-        .await
-        .unwrap();
-
-    let status = rpc_client
-        .get_signature_status(&sig)
-        .await
-        .unwrap()
-        .unwrap();
     assert_eq!(status, Ok(()));
-
-    let statuses = rpc_client
-        .get_signature_statuses(&[sig])
-        .await
-        .unwrap()
-        .value;
     assert_eq!(statuses.len(), 1);
     assert_eq!(statuses[0], successful_transaction_status());
+
+    utils::stop(handle).await;
+}
+
+#[tokio::test]
+async fn test_two_tx_success() {
+    struct AllTxsSuccessMocker;
+    impl SoxMocker for AllTxsSuccessMocker {
+        fn handle_transaction(&self, tx: VersionedTransaction) -> Option<TransactionResult> {
+            debug!("Mocker received transaction: {:?}", tx);
+            Some(TransactionResult::signature_status_success(
+                Signature::new_unique(),
+            ))
+        }
+    }
+
+    let mocker = Arc::new(AllTxsSuccessMocker);
+    let (url, handle) = utils::start(mocker).await.unwrap();
+    let rpc_client = utils::create_rpc_client(&url);
+
+    let tx1 = create_account_tx();
+    let sig1 = rpc_client.send_and_confirm_transaction(&tx1).await.unwrap();
+    let status1 = sig_status(&rpc_client, sig1).await;
+
+    let tx2 = create_account_tx();
+    let sig2 = rpc_client.send_and_confirm_transaction(&tx2).await.unwrap();
+    let status2 = sig_status(&rpc_client, sig2).await;
+
+    assert_eq!(status1, Ok(()));
+    assert_eq!(status2, Ok(()));
+
+    let statuses = sig_statuses(&rpc_client, &[sig1, sig2]).await;
+    assert_eq!(statuses.len(), 2);
+    assert_eq!(statuses[0], successful_transaction_status());
+    assert_eq!(statuses[1], successful_transaction_status());
 
     utils::stop(handle).await;
 }
